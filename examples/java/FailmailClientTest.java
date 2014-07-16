e451cce081706159347c65e2104d43e2b9dd31df
Matt Papi <matt@hut8labs.com>
2014-07-15 22:19:06 -0400

    Add an example Java (log4j) configuration.

---
+++ examples/java/FailmailClientTest.java
@@ -0,0 +1,11 @@
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class FailmailClientTest {
+
+    static final Logger logger = LogManager.getLogger(FailmailClientTest.class.getName());
+
+    public static void main(String[] args) {
+        logger.error("This is Java!!!!");
+    }
+}
