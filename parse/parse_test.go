14f87002c1b153e5c7aa4391fbefc60a29a2cb6f
Matt Papi <matt@hut8labs.com>
2014-08-07 23:40:16 -0400

    Add a parse test for Label.

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -45,3 +45,16 @@ func TestOmit(t *testing.T) {
 		t.Errorf("unexpected parse result: %#v", parsed)
 	}
 }
+
+func TestLabel(t *testing.T) {
+	parser := Label("value", Longest(Literal("test"), Literal("testing")))
+	rest, parsed := parser.Parse("testing 123")
+	if rest != " 123" {
+		t.Errorf("parser left unexpected string: %s", rest)
+	}
+	if parsed == nil || parsed.Text != "testing" {
+		t.Errorf("parsed unexpected fragment: %s", parsed)
+	} else if labeled, ok := parsed.Get("value"); !ok || labeled.Text != "testing" {
+		t.Errorf("unexpected parse result: %s", labeled)
+	}
+}
