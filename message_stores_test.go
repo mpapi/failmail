c443f8f359ebde83371e83a16e0daaea39614615
Matt Papi <matt@hut8labs.com>
2014-10-09 21:51:40 -0400

    Add some tests for message stores.

---
+++ message_stores_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDiskStore(t *testing.T) {
+	maildir, cleanup := makeTestMaildir(t)
+	defer cleanup()
+
+	ds, err := NewDiskStore(maildir)
+	if err != nil {
+		t.Fatalf("couldn't create disk store: %s", err)
+	}
+
+	msg := makeReceivedMessage(t, "From: test@example.com\r\nTo: test@example.com\r\nSubject: test\r\n\r\ntest\r\n")
+	now := time.Unix(1393650000, 0)
+	if err := ds.Add(now, RecipientKey{"test", "test@example.com"}, msg); err != nil {
+		t.Errorf("failed to add message to store: %s", err)
+	}
+
+	results := make([]*ReceivedMessage, 0)
+	calls := 0
+	callback := func(key RecipientKey, loadFunc func() []*ReceivedMessage, softLimit time.Time, hardLimit time.Time) bool {
+		calls += 1
+		for _, msg := range loadFunc() {
+			results = append(results, msg)
+		}
+		return true
+	}
+	if err := ds.Iterate(callback); err != nil {
+		t.Errorf("error on Iterate(): %s", err)
+	} else if calls != 1 {
+		t.Errorf("expected 1 call, got %d", calls)
+	} else if count := len(results); count != 1 {
+		t.Errorf("expected 1 message, got %d", count)
+	}
+
+	if msgs, err := maildir.List(); err != nil {
+		t.Errorf("error on maildir.List(): %s", err)
+	} else if count := len(msgs); count != 0 {
+		t.Errorf("expected empty maildir, found %d entries", count)
+	}
+}
