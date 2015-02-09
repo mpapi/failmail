31bc3029285faff100cbb9651801e1cee83542ba
Matt Papi <matt@hut8labs.com>
2015-02-09 13:45:11 -0500

    Reorganize configuration options.
    Makes it a little easier to see what's what.

--- config.go
+++ config.go
@@ -10,31 +10,37 @@ import (
 )
 
 type Config struct {
-	BindAddr   string        `help:"local bind address"`
-	RelayAddr  string        `help:"relay server address"`
-	SocketFd   int           `help:"file descriptor of socket to listen on"`
-	WaitPeriod time.Duration `help:"wait this long for more batchable messages"`
-	MaxWait    time.Duration `help:"wait at most this long from first message to send summary"`
-	From       string        `help:"from address"`
-	FailDir    string        `help:"write failed sends to this maildir"`
-	AllDir     string        `help:"write all sends to this maildir"`
-	BatchExpr  string        `help:"an expression used to determine how messages are batched into summary emails"`
-	GroupExpr  string        `help:"an expression used to determine how messages are grouped within summary emails"`
-	BindHTTP   string        `help:"local bind address for the HTTP server"`
-	RelayAll   bool          `help:"relay all messages to the upstream server"`
-	Pidfile    string        `help:"write a pidfile to this path"`
-	Template   string        `help:"path to a summary message template file"`
-
+	// Options for listening for incoming messages.
+	BindAddr        string        `help:"local bind address"`
+	SocketFd        int           `help:"file descriptor of socket to listen on"`
+	Credentials     string        `help:"username:password for authenticating to failmail"`
+	TlsCert         string        `help:"PEM certificate file for TLS"`
+	TlsKey          string        `help:"PEM key file for TLS"`
 	ShutdownTimeout time.Duration `help:"wait this long for open connections to finish when shutting down or reloading"`
 
-	RelayUser     string `help:"username for auth to relay server"`
-	RelayPassword string `help:"password for auth to relay server"`
-	Credentials   string `help:"username:password for authenticating to failmail"`
-	TlsCert       string `help:"PEM certificate file for TLS"`
-	TlsKey        string `help:"PEM key file for TLS"`
-
+	// Options for storing messages.
 	MemoryStore  bool   `help:"store messages in memory instead of an on-disk maildir"`
 	MessageStore string `help:"use this directory as a maildir for holding received messages"`
+
+	// Options for summarizing messages.
+	From       string        `help:"from address"`
+	WaitPeriod time.Duration `help:"wait this long for more batchable messages"`
+	MaxWait    time.Duration `help:"wait at most this long from first message to send summary"`
+	BatchExpr  string        `help:"an expression used to determine how messages are batched into summary emails"`
+	GroupExpr  string        `help:"an expression used to determine how messages are grouped within summary emails"`
+	Template   string        `help:"path to a summary message template file"`
+
+	// Options for relaying outgoing messages.
+	RelayAddr     string `help:"upstream relay server address"`
+	RelayAll      bool   `help:"relay all messages to the upstream server"`
+	RelayUser     string `help:"username for auth to relay server"`
+	RelayPassword string `help:"password for auth to relay server"`
+	FailDir       string `help:"write failed sends to this maildir"`
+	AllDir        string `help:"write all sends to this maildir"`
+
+	// Monitoring options.
+	BindHTTP string `help:"local bind address for the HTTP server"`
+	Pidfile  string `help:"write a pidfile to this path"`
 
 	Version bool `help:"show the version number and exit"`
 }
@@ -42,16 +48,20 @@ type Config struct {
 func Defaults() *Config {
 	return &Config{
 		BindAddr:        "localhost:2525",
-		RelayAddr:       "localhost:25",
-		WaitPeriod:      30 * time.Second,
-		MaxWait:         5 * time.Minute,
-		From:            DefaultFromAddress("failmail"),
-		FailDir:         "failed",
-		BatchExpr:       `{{.Header.Get "X-Failmail-Split"}}`,
-		GroupExpr:       `{{.Header.Get "Subject"}}`,
-		BindHTTP:        "localhost:8025",
 		ShutdownTimeout: 5 * time.Second,
-		MessageStore:    "incoming",
+
+		MessageStore: "incoming",
+
+		From:       DefaultFromAddress("failmail"),
+		WaitPeriod: 30 * time.Second,
+		MaxWait:    5 * time.Minute,
+		BatchExpr:  `{{.Header.Get "X-Failmail-Split"}}`,
+		GroupExpr:  `{{.Header.Get "Subject"}}`,
+
+		RelayAddr: "localhost:25",
+		FailDir:   "failed",
+
+		BindHTTP: "localhost:8025",
 	}
 }
 
